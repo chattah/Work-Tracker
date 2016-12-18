e40f5ef2abe0d2a33839e297eff7883bc49febb4
JoelL <JoelL@JOEL-MAIN-DELL>
2016-12-17 20:42:26 -0600

    Add project files.

---
+++ Work Tracker/ExitProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Work_Tracker
+{
+	public static class ExitProgram
+	{
+		public static void Exit()
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
